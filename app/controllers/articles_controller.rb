34b0ff602b2ea4d4f9252cc78253c91db162037b
Kenneth-joseph <josephochieng812@gmail.com>
2021-07-27 18:36:52 +0300

    added the forms and validated it using the strong params and validations on models

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,7 +3,28 @@ class ArticlesController < ApplicationController
     @articles= Article.all
   end
 
+  # displays each article on its own 
   def show
     @article = Article.find(params[:id]) #the id params will help us capture the articles dynamic ids
+  end
+  # creates a new articles by inducing forms
+  def new
+    @article = Article.new
+
+  end
+  # saves the newly created articles into the db, else redirect to the creation page(new)
+  def create
+    @article = Article.new(title: "...", body:"...", author: "...")
+     
+    if  @article.save
+      redirect_to @article
+    else
+      render :new
+    end
+  end
+  # to prevent mulicious users from entering extra fields into the forms and overwriting private data, we'll use the rails feature strong parameters to help filter the params, before create uses the data from the user 
+  private
+  def article_params
+    params.require(:article).permit(:title, :body, :author)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "articles#index" #makng the the index our landing page
-  get "/articles", to: "articles#index"
-  get "/articles/:id", to: "articles#show"  #  this is a route to show a single article 
+
+  #-------
+  # Replacing the routers bellow with a resource
+  # get "/articles", to: "articles#index"
+  # get "/articles/:id", to: "articles#show"  #  this is a route to show a single article 
+
+  #--------
+
+  resources :articles
 end
