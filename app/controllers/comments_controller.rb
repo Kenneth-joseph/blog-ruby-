c1e5e819664980e6f1a35a87e2b6f2c2b21b5c3c
Kenneth-joseph <josephochieng812@gmail.com>
2021-07-29 15:23:44 +0300

    added the comment logic and complete functionality and design

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,14 @@
 class CommentsController < ApplicationController
+
+    # a create method action
+    def create
+        @article = Article.find(params[:article_id]) #find the specific article and store it in the variable article 
+        @comment = @article.comments.create(comment_params) #retrive the article and create a comment for it
+        redirect_to article_path(@article)
+    end
+
+private 
+    def comment_params
+        params.require(:comment).permit(:commenter, :body)
+    end
 end
