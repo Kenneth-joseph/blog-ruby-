99815da57f27cd92aeb531ea8a47338b8aeaf9ee
Kenneth-joseph <josephochieng812@gmail.com>
2021-07-30 01:05:18 +0300

    added the delete button and updated the links on my view , controller and finally model for more effeciency for the action

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,6 +7,16 @@ class CommentsController < ApplicationController
         redirect_to article_path(@article)
     end
 
+    # destroy action 
+    def destroy
+        @article = Article.find(params[:article_id]) #find the specific article and store it in the variable article 
+        @comment = @article.comments.find(params[:id]) # find the comment relatd to the article found above by id
+        @comment.destroy #calls the destroy function on it
+        redirect_to article_path(@article)
+    end
+
+
+
 private 
     def comment_params
         params.require(:comment).permit(:commenter, :body, :status)
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,8 +1,9 @@
 class Article < ApplicationRecord
     #to include the model concern that has the status logic we will include the visibility moodule here.
     include Visibility
-    # update the realtionship between article and the comment model 
-    has_many :comments
+    # update the realtionship between article and the comment model | addition of the dependent and destroy help make sure that each aerticle deleted goes along with its related content, eg comments
+    has_many :comments, dependent: :destroy
+
     #from the forms fields, to ensure the data coming through is validated and the fields are filled s required we validate the fields from the models here
     validates :title, presence: true
     validates :author, presence: true, length: {minimum: 5}
